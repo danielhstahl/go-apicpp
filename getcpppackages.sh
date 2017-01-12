c72682a4d3292c5f5a21e6c46c47386593ba28bf
Daniel Stahl <danstahl1138@gmail.com>
2017-01-11 21:41:33 -0600

    working but not quite launching

--- getcpppackages.sh
+++ getcpppackages.sh
@@ -12,23 +12,29 @@ function cloneAndCheckout {
 
 	git checkout $2
 	cd ..
-	cd personalSite-Ember-Server
+	cd go-apicpp
 }
+
 function compile {
 	cd $1
 	make 
+	cp $1 ../../../../bin
 	cd ..
+
 }
 
 cloneAndCheckout marketRisk 704e069d372220f4cd6ccc39ea0dc2c2d8199f52
 cloneAndCheckout creditRisk c8313965f461b80979e7dbbef94ff680c57bbeb7
 cloneAndCheckout opsRisk 5526f3bd08d4e6e4c21a99aefc0debed12465281
-cloneAndCheckout HullWhite 23081e3974c0bec5e7d735f85eeca15a7c2c7784
+cloneAndCheckout HullWhite 5cee8dec3bf60c93e2099dc4d0add82283231860
+cloneAndCheckout GaussNewton 82fcc7407469aecba142286e28d4b41b0669e707
 cloneAndCheckout FunctionalUtilities 2543852b038436c141a1e3c29bf9c353df9a02b9
+cloneAndCheckout BinomialTree d40d2ebbc9de0e1b7d30c3ec82d55c6d4346ef9a
 cloneAndCheckout MonteCarlo 3d8d757c340bca12a064964eee33d60acd888607
-cloneAndCheckout CharacteristicFunctions 59d5f2e1789bdcfc8d861645ef3d6392de79832f
+cloneAndCheckout CharacteristicFunctions 369e1d54b80a2324bd0e2553adf195f01699bb9e
 cloneAndCheckout FangOost d74b4e541aa5569ccd99192fe3d256e7d23b3883
 cloneAndCheckout RungeKutta b286ce51f5e6957a59a5a49a3d1c60abdd765af4
+cloneAndCheckout AutoDiff d9d79e251f4ab030bcf17fc7b756b4ba06e6fdbd
 cd ..
 git clone https://github.com/miloyip/rapidjson
 compile marketRisk
--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
+//"github.com/abbot/go-http-auth"
+//"log"\
+// "encoding/json"
+//"os/exec"
 import (
-    "github.com/abbot/go-http-auth"
+    
     "log"
     "net/http"
-    "os/exec"
+    "os"
     "io/ioutil"
-    "encoding/json"
+   "encoding/json"
 )
 
-const isDev bool = false
-type marketRiskInput struct{
-    Port string 
-    Date string
-    Amount int
-    Material string
-    Comment string
+const isDev bool = true
+
+type env struct{ //environmental variables
+    Statichtml string
+    
+}
+var currEnv env
+/*type marketRiskInput struct{
+    n int 
+    sigma float64
+    a float64
+    t float64
+    T float64
+    delta float64
+    Tm float64
 }
 type creditRiskInput struct{
-    Port string 
-    Date string
-    Amount int
-    Material string
-    Comment string
+    n int 
+    t float64
+    x0 float64
+    alpha float64
+    sigma float64
+    q float64
+    lambda float64
+    xNum int
+    uNum int
 }
 type opsRiskInput struct{
     Port string 
@@ -50,13 +66,16 @@ type opsRiskOutput struct{
     Amount int
     Material string
     Comment string
-}
-func writePortType(w http.ResponseWriter, r *auth.AuthenticatedRequest){
+}*/
+func handleExecute(w http.ResponseWriter, r *http.Request/**auth.AuthenticatedRequest*/, command string){
     if(isDev){
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
-    decoder := json.NewDecoder(r.Body)
+    log.Println(r.Body)
+    //grepCmd := exec.Command(command, "hello")
+
+    /*decoder := json.NewDecoder(r.Body)
     var t portType   
     err := decoder.Decode(&t)
     if err != nil {
@@ -68,24 +87,46 @@ func writePortType(w http.ResponseWriter, r *auth.AuthenticatedRequest){
     }
     results:=new(success)
     results.Success=true
-    json.NewEncoder(w).Encode(results)
+    json.NewEncoder(w).Encode(results)*/
 }
 
-
+func init(){
+    f, err := os.OpenFile("log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatal("Couldn't open log")
+    }
+    log.SetOutput(f)
+    pwd, _ := os.Getwd()
+    file, err1 := ioutil.ReadFile(pwd+"/environment.json") // For read access
+    if err1!=nil{
+        log.Fatal(err1)
+    }
+    err2:=json.Unmarshal(file,&currEnv)
+    if err2!=nil{
+        log.Fatal(err2)
+    }
+    log.Println(currEnv)
+    
+}
 func main(){
     //defer db.Close()
     
     //ath := auth.NewBasicAuthenticator("example.com", Secret)
-    http.HandleFunc("/", ath.Wrap(func(res http.ResponseWriter, req *auth.AuthenticatedRequest) {
-        http.FileServer(http.Dir(currEnv.Statichtml)).ServeHTTP(res, &req.Request)
-    }))
-    http.HandleFunc("/writePort", ath.Wrap(writePortType))
-    http.HandleFunc("/writeMaterial", ath.Wrap(writeMaterialType))
-    http.HandleFunc("/writeTransaction", ath.Wrap(writeTransaction))
-    http.HandleFunc("/getResults", ath.Wrap(getAsOfMaterials))
-    http.HandleFunc("/getPorts", ath.Wrap(getPort))
-    http.HandleFunc("/getMaterials", ath.Wrap(getMaterial))
-    http.HandleFunc("/getAllResults", ath.Wrap(getAllResults))
+    //http.HandleFunc("/", /*ath.Wrap(func(res http.ResponseWriter, req *auth.AuthenticatedRequest*/func(res http.ResponseWriter, req *http.Request) {
+        //http.FileServer(http.Dir(currEnv.Statichtml)).ServeHTTP(res, &req.Request)
+    //})
+    log.Println(currEnv.Statichtml)
+    http.Handle("/", http.FileServer(http.Dir(currEnv.Statichtml)))
+    http.HandleFunc("/RunModel/creditRisk", func(w http.ResponseWriter, r *http.Request){
+        handleExecute(w, r, "./creditRisk")
+    })
+    http.HandleFunc("/RunModel/marketRisk", func(w http.ResponseWriter, r *http.Request){
+        handleExecute(w, r, "./marketRisk")
+    })
+    http.HandleFunc("/RunModel/opsRisk", func(w http.ResponseWriter, r *http.Request){
+        handleExecute(w, r, "./opsRisk")
+    })
     log.Println("Listening...")
-    http.ListenAndServe(":3000", nil)
+    http.ListenAndServe(":3001", nil)
+     
 }
